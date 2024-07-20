197dd0b392bb3eff955dc489320391d7bfe27978
tyty <tyty.jenkins.game@gmail.com>
2024-07-20 21:21:28 +0200

    add form login and register design

--- view/src/views/LoginView.vue
+++ view/src/views/LoginView.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
-    <p>Login view</p>
+    <div class="row justify-content-content align-center h-100">
+        <form class="form col-4">
+            <div class="form-group">
+                <label for="inputLoginEmail" class="form-label">Identifiant</label>
+                <input type="email" class="form-control" id="inputLoginEmail" aria-describedby="emailHelp">
+            </div>
+
+            <div class="form-group">
+                <label for="inputLoginPassword" class="form-label">Mot de passe</label>
+                <input type="password" class="form-control" id="inputLoginPassword">
+            </div>
+            
+            <div class="btn-group justify-content-end">
+                <button type="submit" class="btn btn-primary">Connexion</button>
+                <RouterLink to="/register" class="btn btn-outline">Inscription</RouterLink>
+            </div>
+        </form>
+    </div>
 </template>
--- view/src/views/RegisterView.vue
+++ view/src/views/RegisterView.vue
@@ -1,3 +1,26 @@
 <template>
-    <p>Register view</p>
+    <div class="row justify-content-content align-center h-100">
+        <form class="form col-4">
+            <div class="form-group">
+                <label for="inputRegisterEmail" class="form-label">Identifiant</label>
+                <input type="email" class="form-control" id="inputRegisterEmail" aria-describedby="emailHelp">
+            </div>
+
+            <div class="form-group">
+                <label for="inputRegisterPassword" class="form-label">Mot de passe</label>
+                <input type="password" class="form-control" id="inputRegisterPassword">
+                <p>Votre mot de passe doit contenir</p>
+                <p>Au moins 10 caractèresUne minusculeUne majusculeUn chiffreUn caractère spécial</p>
+            </div>
+
+            <div class="form-group">
+                <label for="inputRegisterPassword2" class="form-label">Confirmez le mot de passe</label>
+                <input type="password" class="form-control" id="inputRegisterPassword2">
+            </div>
+            
+            <div class="btn-group justify-content-end">
+                <button type="submit" class="btn btn-primary">S'inscrire</button>
+            </div>
+        </form>
+    </div>
 </template>
