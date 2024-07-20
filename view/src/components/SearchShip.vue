ec7ba3883a97b82f55455ef57686821f4dc43c57
tyty <tyty.jenkins.game@gmail.com>
2024-07-21 00:20:42 +0200

    add feat tab

---
+++ view/src/components/SearchShip.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>Tab ship</p>
+</template>
--- view/src/views/SearchView.vue
+++ view/src/views/SearchView.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import { ref, computed, onMounted } from 'vue'
+import SearchPlanet from '../components/SearchPlanet.vue'
+import SearchShip from '../components/SearchShip.vue'
+
+const idActive = ref<string>('active');
+const pannels = ref([]);
+
+onMounted(() => {
+    pannels.value = Array.from(document.querySelectorAll('[data-tab="pannel"]')); 
+})
+
+const tabActivate = (id: string) => idActive.value === id ? 'active' : '';
+
+const activateTab = (id: string) => {
+    idActive.value = id;
+    pannels.value.map((item: HTMLElement) => {
+        item.id == id ? item.classList.remove('d-none') : item.classList.add('d-none')    
+    })
+
+}
+</script>
+
 <template>
-    <p>Search view</p>
+    <nav class="nav-tab">
+        <div class="nav-item" :class="[tabActivate('shipTab'), idActive]" @click="activateTab('shipTab')">Tab Ship</div>
+        <div class="nav-item" :class="tabActivate('planetTab')" @click="activateTab('planetTab')">Tab Planet</div>
+    </nav>
+
+    <div class="tab-content">
+        <div id="shipTab" class="tab-panel" data-tab="pannel">panel ship</div>
+        <div id="planetTab" class="tab-panel d-none" data-tab="pannel">panel planet</div>
+    </div>
 </template>
